e2c87eae3b1442c78e1e76f2289f60e47c6fe391
Jay Zangwill <359390851@qq.com>
2018-05-15 17:13:07 +0800

    修改路径添加删除dist功能

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -74,7 +74,7 @@ module.exports = new Promise((resolve, reject) => {
       // Add FriendlyErrorsPlugin
       devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
         compilationSuccessInfo: {
-          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
+          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}/module`],
         },
         onErrors: config.dev.notifyOnErrors ?
           utils.createNotifierCallback() : undefined
--- config/index.js
+++ config/index.js
@@ -40,7 +40,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '../',
 
     /**
      * Source Maps
